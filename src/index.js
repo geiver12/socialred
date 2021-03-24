d3af8496c2270b65523454d27340af208104fd16
geiver12 <sosdani2000@hotmail.com>
2021-03-24 17:27:39 -0400

    new functions and database

--- src/index.js
+++ src/index.js
@@ -54,6 +54,13 @@ app.use(passport.session());
 app.use(flash());
 
 
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+  }
+  
+  const DB_URI = process.env.DB_URI
+  const PORT = process.env.PORT
+
 //Global Variables
 
 app.use((req, res, next) => {
